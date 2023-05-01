1712da6b373766167710ee5685b6c7d662c5bb8e
taku-0728 <taku8270@gmail.com>
2023-05-01 23:57:26 +0900

    [add] server.go

---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type Server struct {
+	srv *http.Server
+	l   net.Listener
+}
+
+func NewServer(l net.Listener, mux http.Handler) *Server {
+	return &Server{
+		srv: &http.Server{Handler: mux},
+		l:   l,
+	}
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.srv.Serve(s.l); err != nil &&
+			// http.ErrServerClosed は
+			// http.Server.Shutdown()が正常に終了したことを示すので異常ではない。
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+
+	})
+
+	// チャネルからの通知(終了通知)を待機する
+	<-ctx.Done()
+	if err := s.srv.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別ゴルーチンの終了を待つ。
+	return eg.Wait()
+}
