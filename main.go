42bbaa3a4b719ea78d85418a695a64b6c15a749d
taku-0728 <taku8270@gmail.com>
2023-05-04 22:15:19 +0900

    [mod] ルーティング定義を分割する

--- main.go
+++ main.go
@@ -1,52 +1,34 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/taku-0728/go_todo_app/config"
+)
+
 func main() {
-	// if err := run(context.Background()); err != nil {
-	// 	log.Printf("failed to terminate server: %v", err)
-	// 	os.Exit(1)
-	// }
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
 
-// func run(ctx context.Context) error {
-// 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-// 	defer stop()
-// 	cfg, err := config.New()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
-// 	if err != nil {
-// 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
-// 	}
-// 	url := fmt.Sprintf("http://%s", l.Addr().String())
-// 	log.Printf("start with: %v", url)
-// 	s := &http.Server{
-// 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 			// コマンドラインで実験するため
-// 			time.Sleep(5 * time.Second)
-// 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-// 		}),
-// 	}
-
-// 	eg, ctx := errgroup.WithContext(ctx)
-// 	// 別ゴルーチンでHTTPサーバを起動する
-// 	eg.Go(func() error {
-// 		if err := s.Serve(l); err != nil &&
-// 			// http.ErrServerClosed は
-// 			// http.Server.Shutdown()が正常に終了したことを示すので異常ではない。
-// 			err != http.ErrServerClosed {
-// 			log.Printf("failed to close: %+v", err)
-// 			return err
-// 		}
-// 		return nil
-
-// 	})
-
-// 	// チャネルからの通知(終了通知)を待機する
-// 	<-ctx.Done()
-// 	if err := s.Shutdown(context.Background()); err != nil {
-// 		log.Printf("failed to shutdown: %+v", err)
-// 	}
-// 	// Goメソッドで起動した別ゴルーチンの終了を待つ。
-// 	return eg.Wait()
-// }
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
+}
