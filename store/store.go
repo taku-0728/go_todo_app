6403e94f9ecc3ec2e2fca8719d8e2c07ebe82a0f
taku-0728 <taku8270@gmail.com>
2023-05-05 11:54:04 +0900

    [add] データをメモリ上に保存するメソッドを追加

---
+++ store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/taku-0728/go_todo_app/entity"
+)
+
+var (
+	Tasks       = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	// 動作確認用の仮実装なのであえてexportしている。
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+// Allはソート済みのタスク一覧を返す
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
