c15886b6a7df26541c723ccd7fe514fe5d117522
taku-0728 <taku8270@gmail.com>
2023-05-11 23:16:57 +0900

    [del] 不要な差分を削除

--- mux.go
+++ mux.go
@@ -10,13 +10,6 @@ import (
 )
 
 func NewMux() http.Handler {
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Header().Set("Content-Type", "application.json; charset=utf-8")
-	// 	// 静的解析のエラーを回避するため明示的に戻り値を捨てている
-	// 	_, _ = w.Write([]byte(`{"status": "ok"}`))
-	// })
-	// return mux
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-type", "application/json: charset=utf-8")
