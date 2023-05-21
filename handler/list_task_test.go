eed0c663f62183ecc869168871b899a5c4d32f70
taku-0728 <taku8270@gmail.com>
2023-05-21 22:32:31 +0900

    [fix] test

--- handler/list_task_test.go
+++ handler/list_task_test.go
@@ -35,14 +35,14 @@ func TestListTask(t *testing.T) {
 			},
 			want: want{
 				status:  http.StatusOK,
-				rspFile: "testdata/ok_rsp.json.golden",
+				rspFile: "testdata/list_task/ok_rsp.json.golden",
 			},
 		},
 		"empty": {
 			tasks: []*entity.Task{},
 			want: want{
 				status:  http.StatusOK,
-				rspFile: "testdata/empty_rsp.json.golden",
+				rspFile: "testdata/list_task/empty_rsp.json.golden",
 			},
 		},
 	}
